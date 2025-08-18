c3a15c1355ddfe79cf2987595be6a899882200c3
Gannanosal <19780101i.ua@gmail.com>
2025-08-18 16:07:06 +0200

    add sale block

---
+++ src/components/sale/Sale.jsx
@@ -0,0 +1,20 @@
+
+import "./Sale.scss";
+
+export default function Sale() {
+      console.log('Sale mounted');
+
+  return (
+    <section className="sale">
+      <div className="sale-header">
+        <h2 className="sale-title">Sale</h2>
+        <div></div>
+        <button className="sale-btn">All sales</button>
+      </div>
+      <div className="line" />
+    
+      
+    </section>
+  );
+}
+
