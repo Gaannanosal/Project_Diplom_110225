a9604c747712189ae453590ab23426a57e99bde0
Gannanosal <19780101i.ua@gmail.com>
2025-08-18 19:56:36 +0200

    add discount slice

--- src/components/diskountForm/DiskountForm.jsx
+++ src/components/diskountForm/DiskountForm.jsx
@@ -1,77 +1,155 @@
-import React, { useState } from "react";
-import "./DiskountForm.scss";
+import { useForm } from "react-hook-form";
 import banner from "../../assets/DiscountBanner.png";
 
-const initialForm = { name: "", phone: "", email: "" };
+import "./DiskountForm.scss";
+import { useDispatch, useSelector } from "react-redux";
+import { sendDiscountRequest } from "../../store/features/discountSlice";
+import { useEffect, useState } from "react";
+import { FaTimesCircle } from "react-icons/fa";
 
-export default function DiscountForm() {
-  const [form, setForm] = useState(initialForm);
-  const [sent, setSent] = useState(false);
+const savedForm = JSON.parse(localStorage.getItem("cart-form-discount")) || {
+  name: "",
+  phone: "",
+  email: "",
+};
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setForm((p) => ({ ...p, [name]: value }));
+const DiscountForm = () => {
+  const dispatch = useDispatch();
+  const [isDisabledSend, setIsDisabledSend] = useState(false); // временно отключает кнопку отправки после одного отправления.
+  const { message, loading, success, error } = useSelector(
+    (state) => state.discount
+  );
+
+  const {
+    register, // связывает инпуты с системой валидации
+    formState: { errors },
+    handleSubmit,
+    watch, // отслеживает изменения полей формы.
+    reset,
+  } = useForm({
+    mode: "onBlur", // валидация срабатывает при потере фокуса.
+    defaultValues: savedForm,
+  });
+
+  useEffect(() => {
+    const subscription = watch((value) => {
+      localStorage.setItem("cart-form-discount", JSON.stringify(value));
+    });
+    return () => subscription.unsubscribe(); // функция очистки
+  }, [watch]); // следим за каждым изменением формы
+
+  const onSubmit = (data) => {
+    // вызывается при успешной валидации
+    reset({
+      name: "",
+      phone: "",
+      email: "",
+    });
+    setIsDisabledSend(true); // блокирует кнопку
+    dispatch(sendDiscountRequest(data)); // oтправляет данные на сервер
   };
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    console.log("Discount form:", form);
-    setSent(true);
-    setForm(initialForm);
-  };
+  const formValidationErrors = Object.values(errors).map((err) => err.message);
+  const hasFormValidationErrors = formValidationErrors.length > 0;
+  // собираем массив сообщений об ошибках и определяем, есть ли вообще ошибки
 
   return (
-    <section className="discount">
-      <div className="discount__inner">
-        <h2 className="discount__title">5% off on the first order</h2>
+    <div className="discount">
+      <div className="discount-banner">
+        <h2 className="discount-banner__title">5% off on the first order</h2>
 
-        <div className="discount__content">
-          
-            <img className="discount__image" src={banner} alt="First order discount" />
-        
-
-          <form className="discount__form" onSubmit={onSubmit}>
-            <label className="discount__label">
+        <div className="discount-banner__content">
+          <form
+            className="discount-banner__form"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="discount-banner__inputs">
               <input
-                className="discount__input"
                 type="text"
                 name="name"
                 placeholder="Name"
-                value={form.name}
-                onChange={onChange}
-                required
+                {...register("name", {
+                  required: "Name is required",
+                  minLength: {
+                    value: 3,
+                    message: "Minimum two characters",
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: "Name is too long",
+                  },
+                  pattern: {
+                    value: /^[A-Za-zА-Яа-я\s'-]+$/,
+                    message: "Invalid name format",
+                  },
+                })}
+                className={errors.name ? "input-error" : ""}
               />
-            </label>
 
-            <label className="discount__label">
               <input
-                className="discount__input"
                 type="tel"
                 name="phone"
                 placeholder="Phone number"
-                value={form.phone}
-                onChange={onChange}
-                required
+                {...register("phone", {
+                  required: "Phone number is required",
+                  pattern: {
+                    value: /^\+?(\d[\s-]?)?(\(?\d{3}\)?[\s-]?)?[\d\s-]{7,15}$/,
+                    message: "Invalid phone number format",
+                  },
+                })}
+                className={errors.phone ? "input-error" : ""}
               />
-            </label>
 
-            <label className="discount__label">
               <input
-                className="discount__input"
                 type="email"
                 name="email"
                 placeholder="Email"
-                value={form.email}
-                onChange={onChange}
-                required
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Invalid email format",
+                  },
+                })}
+                className={errors.email ? "input-error" : ""}
               />
-            </label>
 
-            <button className="discount__btn" type="submit">Get a discount</button>
-           
+              <div className="error-content">
+                {hasFormValidationErrors && !loading && !success && !error && (
+                  <p className="form-general-error-message error-box">
+                    {FaTimesCircle && (
+                      <FaTimesCircle size={16} color="#dc3545" />
+                    )}
+                    {formValidationErrors.join(". ")}
+                  </p>
+                )}
+
+                {success && message && (
+                  <p className="success-message">{message}</p>
+                )}
+                {error && <p className="api-error-message">{error}</p>}
+              </div>
+            </div>
+
+            <div
+              className={
+                "discount-banner__button " + (success ? "success" : "")
+              }
+            >
+              <button type="submit" disabled={isDisabledSend}>
+                {success ? "Request Submitted" : "Get a discount"}
+              </button>
+            </div>
+            <img
+              src={banner}
+              className="discount-banner__form-img"
+              alt="discountImage"
+            />
           </form>
         </div>
       </div>
-    </section>
+    </div>
   );
-}
+};
+
+export default DiscountForm;
--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,20 +1,18 @@
-
+import { NavLink } from "react-router-dom";
 import "./Sale.scss";
 
 export default function Sale() {
-      console.log('Sale mounted');
-
   return (
-    <section className="sale">
+    <div className="sale">
       <div className="sale-header">
         <h2 className="sale-title">Sale</h2>
-        <div></div>
-        <button className="sale-btn">All sales</button>
+        <div className="sale__linie-wrapper">
+          <div className="sale__linie"></div>
+          <NavLink to="/sale">
+            <div className="sale__btn">All sales</div>
+          </NavLink>
+        </div>
       </div>
-      <div className="line" />
-    
-      
-    </section>
+    </div>
   );
 }
-
